e72d5beaf37d5a9f720976afe77a18b251104ee9
Kyle Kloberdanz <kyle@capeprivacy.com>
2022-07-25 11:39:54 -0500

    added search get and contains

--- bst.py
+++ bst.py
@@ -2,24 +2,25 @@
 
 
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
         self.left = None
         self.right = None
 
     def __repr__(self):
-        return str(self.data)
+        return f"{repr(self.key)}: {repr(self.value)}"
 
 
 class BST:
     def __init__(self):
         self.head = None
 
-    def insert(self, data):
+    def insert(self, key, value):
         if self.head is None:
-            self.head = Node(data)
+            self.head = Node(key, value)
         else:
-            _insert(self.head, data)
+            _insert(self.head, key, value)
 
     def inorder_traverse(self):
         acc = []
@@ -36,10 +37,30 @@ class BST:
         _postorder_traverse(self.head, acc)
         return acc
 
+    def get(self, key):
+        return _search(self.head, key)
+
+    def __contains__(self, key):
+        return self.get(key) is not None
+
+
+def _search(node, key):
+    if node is None:
+        return None
+
+    if key == node.key:
+        return node
+
+    if key < node.key:  # go left
+        return _search(node.left, key)
+
+    if key > node.key:  # go right
+        return _search(node.right, key)
+
 
 def _preorder_traverse(node, acc):
     if node is not None:
-        acc.append(node.data)
+        acc.append(node)
         _preorder_traverse(node.left, acc)
         _preorder_traverse(node.right, acc)
 
@@ -47,7 +68,7 @@ def _preorder_traverse(node, acc):
 def _inorder_traverse(node, acc):
     if node is not None:
         _inorder_traverse(node.left, acc)
-        acc.append(node.data)
+        acc.append(node)
         _inorder_traverse(node.right, acc)
 
 
@@ -55,30 +76,35 @@ def _postorder_traverse(node, acc):
     if node is not None:
         _postorder_traverse(node.left, acc)
         _postorder_traverse(node.right, acc)
-        acc.append(node.data)
+        acc.append(node)
 
 
-def _insert(node, data):
-    if data < node.data:  # go left
+def _insert(node, key, value):
+    if key < node.key:  # go left
         if node.left is None:  # do insert
-            node.left = Node(data)
+            node.left = Node(key, value)
         else:
-            _insert(node.left, data)
-    elif data > node.data:  # go right
+            _insert(node.left, key, value)
+    elif key > node.key:  # go right
         if node.right is None:  # do insert
-            node.right = Node(data)
+            node.right = Node(key, value)
         else:
-            _insert(node.right, data)
+            _insert(node.right, key, value)
 
 
 if __name__ == "__main__":
     bst = BST()
-    bst.insert(4)
-    bst.insert(3)
-    bst.insert(8)
-    bst.insert(1)
-    bst.insert(2)
-    bst.insert(9)
+    bst.insert("four", 4)
+    bst.insert("three", 3)
+    bst.insert("eight", 8)
+    bst.insert("one", 1)
+    bst.insert("two", 2)
+    bst.insert("nine", 9)
     print(bst.preorder_traverse())
     print(bst.inorder_traverse())
     print(bst.postorder_traverse())
+    print(bst.get("nine"))
+    print(bst.get("zero"))
+    print(bst.get("two"))
+    print("four" in bst)
+    print("zero" in bst)
